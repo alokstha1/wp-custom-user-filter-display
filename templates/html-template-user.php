7e2103d37a46ae0421cb52dae12ab980d2d04ecc
Alok Shrestha <alokstha1@gmail.com>
2019-02-28 15:23:41 +0545

    File which contains the main loop of users query

--- templates/html-template-user.php
+++ templates/html-template-user.php
@@ -4,38 +4,42 @@
 */
 ?>
 <!-- Start of the user listing table -->
-<table class="wp-list-table widefat fixed striped users wcufd-users-list">
-    <thead>
-        <tr>
-            <th><?php echo __( 'Display Name', 'wcufd' ); ?></th>
-            <th><?php echo __( 'Username', 'wcufd' ); ?></th>
-            <th><?php echo __( 'Email', 'wcufd' ); ?></th>
-        </tr>
-    </thead>
+<?php
+if ( !empty( $user_query ) ) {
+?>
+    <table class="wp-list-table widefat fixed striped users wcufd-users-list">
+        <thead>
+            <tr>
+                <th><?php echo __( 'Display Name', 'wcufd' ); ?></th>
+                <th><?php echo __( 'Username', 'wcufd' ); ?></th>
+                <th><?php echo __( 'Email', 'wcufd' ); ?></th>
+            </tr>
+        </thead>
 
-    <tbody id="wcufd-the-list">
-        <?php
-        if ( !empty( $user_query ) ) {
-            foreach ( $user_query as $user_value ) {
-                $user_login         = $user_value->user_login;
-                $user_email         = $user_value->user_email;
-                ?>
-                <tr>
-                    <td><?php echo $user_value->display_name; ?></td>
-                    <td><?php echo $user_login; ?></td>
-                    <td><?php echo $user_email; ?></td>
-                </tr>
-                <?php
-            }
-        } else {
-            // display empty message
-            ?>
-            <tr><td><?php echo __( 'No user found!', 'wcufd' ); ?></td></tr>
+        <tbody id="wcufd-the-list">
             <?php
-        }
-        ?>
-    </tbody>
-</table>
+                foreach ( $user_query as $user_value ) {
+                    $user_login         = $user_value->user_login;
+                    $user_email         = $user_value->user_email;
+                    ?>
+                    <tr>
+                        <td><?php echo $user_value->display_name; ?></td>
+                        <td><?php echo $user_login; ?></td>
+                        <td><?php echo $user_email; ?></td>
+                    </tr>
+                    <?php
+                }
+            ?>
+        </tbody>
+    </table>
+<?php
+ } else {
+        // display empty message
+    ?>
+    <h2><?php echo __( 'No user found!', 'wcufd' ); ?></h2>
+    <?php
+}
+?>
 <!-- End of the user listing table -->
 
 <?php
