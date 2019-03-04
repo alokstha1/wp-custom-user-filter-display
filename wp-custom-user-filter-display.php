20a7dd6b6174afafefc4816f2e0fb7539c4e28c5
Alok Shrestha <alokstha1@gmail.com>
2019-03-04 13:37:51 +0545

    Changed into the main file for enqueue and other functionalities changes

--- wp-custom-user-filter-display.php
+++ wp-custom-user-filter-display.php
@@ -42,12 +42,13 @@ if (! class_exists('WpCustomUserFilterDisplay')) {
         */
         public function wcufd_load_styles_scripts() {
             //main script file of the plugin
-            wp_enqueue_script( 'wcufd-front-end-js', plugins_url( 'js/wcufd-script.js',__FILE__ ), array('jquery'), '', true );
+            wp_register_script( 'wcufd-front-end-js', plugins_url( 'js/wcufd-script.js',__FILE__ ), array('jquery'), '', true );
             wp_localize_script( 'wcufd-front-end-js', 'wcufd_vars', array(
-                'ajaxurl'   => admin_url('admin-ajax.php')
+                'ajaxurl'           => admin_url('admin-ajax.php'),
+                'current_user_can'  => current_user_can('administrator')
                 )
             );
-            wp_enqueue_style( 'wcufd-front-end-style', plugins_url( 'css/wcufd-style.css',__FILE__ ) );
+            wp_register_style( 'wcufd-front-end-style', plugins_url( 'css/wcufd-style.css',__FILE__ ) );
         }
 
         /**
@@ -60,8 +61,8 @@ if (! class_exists('WpCustomUserFilterDisplay')) {
                 $number         = 10; //max display per page
                 $paged          = (get_query_var('paged')) ? get_query_var('paged') : 1; //current number of page
                 $offset         = ($paged - 1) * $number; //page offset
-                $users          = get_users(); //get all the lists of users
-                $total_users    = count($users);//count total users
+                $count_users    = count_users(); // get the count of users
+                $total_users    = $count_users['total_users'];//count total users
                 $total_pages    = ($total_users / $number); // get the total pages by dividing the total users to the maximum numbers of user to be displayed
                 $total_pages    = is_float($total_pages) ? intval($total_users / $number) + 1 : intval($total_users / $number); //Check if the total pages has a decimal we will add + 1 page
                 $args           = array(
@@ -71,7 +72,8 @@ if (! class_exists('WpCustomUserFilterDisplay')) {
                 $user_query     = get_users($args);
                 $total_query    = count($user_query);//count the maximum displayed users
 
-
+                wp_enqueue_script('wcufd-front-end-js'); //enqueue script here with the shortcode
+                wp_enqueue_style('wcufd-front-end-style'); //enqueue tyle here with the shortcode
                 echo '<div class="wcufd-container">';
                 echo '<div class="wcufd-loading"></div>'; //loader class element
                 echo '<div class="wcufd-filter-container">';
@@ -88,61 +90,69 @@ if (! class_exists('WpCustomUserFilterDisplay')) {
         }
 
         public function wcufd_ajax_filter_user() {
+            if ( current_user_can('administrator') ) {
 
-            if ( !isset( $_POST['validate_submit'] ) || !wp_verify_nonce( $_POST['validate_submit'], 'wcufd_nonce_users' ) ) {
-                // validate nonce & return false if not validated
-                $return = array(
-                    'status'    => false,
-                    'response_html'  => __( 'Something went wrong! Please try again.', 'wcufd' )
-                );
-            } else {
-
-                ob_start();
-                $paged          = ( isset( $_POST['paged'] ) && !empty( $_POST['paged'] ) ) ? intval( $_POST['paged'] ) : 1;
-                $number         = 10; //max display per page
-                $offset         = ($paged - 1) * $number; //page offset
-
-                // if role filter is used
-                if ( isset( $_POST['users_role'] ) && !empty( $_POST['users_role'] ) ) {
-
-                    $role           = sanitize_text_field( $_POST['users_role'] );
-                    $args['role']   = $role;
-                    $users          = get_users( array( 'role'  => $role ) ); //count total users based on roles for using in pagination
-
+                if ( !isset( $_POST['validate_submit'] ) || !wp_verify_nonce( $_POST['validate_submit'], 'wcufd_nonce_users' ) ) {
+                    // validate nonce & return false if not validated
+                    $return = array(
+                        'status'    => false,
+                        'response_html'  => __( 'Something went wrong! Please try again.', 'wcufd' )
+                    );
                 } else {
 
-                    $users      = get_users(); //count total users for using in pagination
+                    ob_start();
+                    $paged          = ( isset( $_POST['paged'] ) && !empty( $_POST['paged'] ) ) ? intval( $_POST['paged'] ) : 1;
+                    $number         = 10; //max display per page
+                    $offset         = ($paged - 1) * $number; //page offset
+                    $count_users    = count_users(); // get the count of users
+
+                    // if role filter is used
+                    if ( isset( $_POST['users_role'] ) && !empty( $_POST['users_role'] ) ) {
+
+                        $role           = sanitize_text_field( $_POST['users_role'] );
+                        $args['role']   = $role;
+                        $total_users    = ( isset( $count_users['avail_roles'][$role] ) && !empty( $count_users['avail_roles'][$role] ) ) ? $count_users['avail_roles'][$role] : 0;//count total users based on roles for using in pagination
+
+                    } else {
+
+                        $total_users    = $count_users['total_users'];//count total users
+                    }
+
+                    $total_pages    = ($total_users / $number); // get the total pages by dividing the total users to the maximum numbers of user to be displayed
+                    $total_pages    = is_float($total_pages) ? intval($total_users / $number) + 1 : intval($total_users / $number); //Check if the total pages has a decimal we will add + 1 page
+                    $args['offset'] = $offset;
+                    $args['number'] = $number;
+
+                    // order by Ascending or Descending
+                    if ( isset( $_POST['users_order'] ) && !empty( $_POST['users_order'] ) ) {
+
+                        $order          = sanitize_text_field( $_POST['users_order'] );
+                        $args['order']  = $order;
+
+                    }
+
+                    // if orderby parameter is passed
+                    if ( isset( $_POST['users_orderby'] ) && !empty( $_POST['users_orderby'] ) ) {
+                        $order_by           = sanitize_text_field( $_POST['users_orderby'] );
+                        $args['orderby']    = $order_by;
+                    }
+
+                    $user_query     = get_users($args); //main user query to list based on the passed arguments
+                    $total_query    = count($user_query);//count the maximum displayed users
+
+                    // Template included with html loop for above query
+                    include_once( plugin_dir_path( __FILE__ ). 'templates/html-template-user.php' );
+
+                    $html_result = ob_get_clean();
+                    $return = array(
+                        'status'        => true,
+                        'response_html' => $html_result
+                    );
                 }
-                $total_users    = count($users);//count total users
-                $total_pages    = ($total_users / $number); // get the total pages by dividing the total users to the maximum numbers of user to be displayed
-                $total_pages    = is_float($total_pages) ? intval($total_users / $number) + 1 : intval($total_users / $number); //Check if the total pages has a decimal we will add + 1 page
-                $args['offset'] = $offset;
-                $args['number'] = $number;
-
-                // order by Ascending or Descending
-                if ( isset( $_POST['users_order'] ) && !empty( $_POST['users_order'] ) ) {
-
-                    $order          = sanitize_text_field( $_POST['users_order'] );
-                    $args['order']  = $order;
-
-                }
-
-                // if orderby parameter is passed
-                if ( isset( $_POST['users_orderby'] ) && !empty( $_POST['users_orderby'] ) ) {
-                    $order_by           = sanitize_text_field( $_POST['users_orderby'] );
-                    $args['orderby']    = $order_by;
-                }
-
-                $user_query     = get_users($args); //main user query to list based on the passed arguments
-                $total_query    = count($user_query);//count the maximum displayed users
-
-                // Template included with html loop for above query
-                include_once( plugin_dir_path( __FILE__ ). 'templates/html-template-user.php' );
-
-                $html_result = ob_get_clean();
+            } else {
                 $return = array(
                     'status'        => true,
-                    'response_html' => $html_result
+                    'response_html' => __( 'You must be logged in as Admin to perform filter!', 'wcufd' )
                 );
             }
             wp_send_json( $return ); // Send a JSON response back and die().
